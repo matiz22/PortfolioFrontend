1dc1db6e975570d67105b03be6e8564657d30f66
matiz22 <mateuszmalich@gmail.com>
2025-08-26 21:00:10 +0200

    add about me component with template, styles

---
+++ src/app/features/about-me/about-me.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-me',
+    imports: [
+    ],
+  templateUrl: './about-me.html',
+  styleUrl: './about-me.css'
+})
+export class AboutMe {
+
+}
