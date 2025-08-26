b481764669f92164f1ff09ff562894723624c75a
matiz22 <mateuszmalich@gmail.com>
2025-08-26 21:01:30 +0200

    add app header with toolbar and main layout styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatToolbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
