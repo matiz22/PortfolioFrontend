4d53bfbcded6d4ff83251e6c6e5c258a8c9093a2
matiz22 <mateuszmalich@gmail.com>
2025-08-26 21:00:44 +0200

    add home page component with template, styles

---
+++ src/app/features/home-page/home-page.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+import {HeroSection} from '../../shared/hero/hero-section/hero-section';
+import {AboutMe} from '../about-me/about-me';
+import {MatDivider} from '@angular/material/divider';
+import {MatIconButton} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@Component({
+  selector: 'app-home-page',
+  imports: [
+    HeroSection,
+    AboutMe,
+    MatDivider,
+    MatIconButton,
+    MatIconModule
+  ],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.css'
+})
+export class HomePage {
+  cardTitle: string = $localize`:@@homeTitle:👋 Hi, I’m Mateusz — Mobile & Web Developer`;
+  cardSubtitle: string = $localize`:@@homeSubtitle:Crafting simple, effective digital experiences.`;
+}
