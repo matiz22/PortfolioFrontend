c14ab6ce8548cdc552324868cae5dd7ea05e1b97
matiz22 <mateuszmalich@gmail.com>
2025-08-26 21:01:37 +0200

    add routing for home page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomePage} from './features/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  }
+
+];
