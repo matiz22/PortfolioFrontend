6ca97763b04b8552a45cad767b24aeb9ef72105e
matiz22 <mateuszmalich@gmail.com>
2025-08-27 11:23:32 +0200

    add sidenav component with smooth scrolling and toggle functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,44 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatToolbar} from '@angular/material/toolbar';
+import {
+  MatDrawer, MatDrawerContainer,
+  MatDrawerContent, MatSidenav, MatSidenavContainer, MatSidenavContent,
+} from '@angular/material/sidenav';
+import {MatListItem, MatNavList} from '@angular/material/list';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbar, MatToolbar],
+  imports: [RouterOutlet, MatToolbar, MatToolbar, MatNavList, MatListItem, MatButton, MatDrawerContent, MatDrawer, MatDrawerContainer, MatIconButton, MatIconModule, MatSidenavContainer, MatSidenav, MatSidenavContent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('PortfolioFrontend');
+
+  @ViewChild('drawer') drawer!: MatSidenav;
+
+  navigateToSection(sectionId: string, drawer: MatSidenav): void {
+    drawer.close();
+
+    setTimeout(() => {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 300);
+  }
+
+  toggleDrawer(drawer: MatSidenav): void {
+    drawer.toggle();
+
+    setTimeout(() => {
+      if (document.activeElement instanceof HTMLElement) {
+        document.activeElement.blur();
+      }
+    });
+  }
+
 }
