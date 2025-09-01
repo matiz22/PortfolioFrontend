2adbc06c7aa6f0cebc3eef2fff0f0b821e2ab1f6
matiz22 <mateuszmalich@gmail.com>
2025-09-01 13:40:54 +0200

    enhance app configuration with custom route reuse strategy and HTTP client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,37 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter, RouteReuseStrategy, withInMemoryScrolling, withViewTransitions} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {routes} from './app.routes';
+import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
+import {ProjectService} from './core/services/project.service';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {MAT_RIPPLE_GLOBAL_OPTIONS} from '@angular/material/core';
+import {CustomRouteReuseStrategy} from './route.reuse.strategy';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    {
+      provide: MAT_RIPPLE_GLOBAL_OPTIONS,
+      useValue: {
+        animation: {
+          enterDuration: 1000,
+          exitDuration: 1000
+        }
+      }
+    },
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(
+      routes,
+      withViewTransitions(),
+      withInMemoryScrolling({
+          scrollPositionRestoration: 'enabled',
+        }
+      ),
+    ),
+    {provide: RouteReuseStrategy, useClass: CustomRouteReuseStrategy},
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+    ProjectService
   ]
 };
