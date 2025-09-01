8a4075b2ba25d476267374a0e5b4e6d12d1a3b31
matiz22 <mateuszmalich@gmail.com>
2025-09-01 13:43:53 +0200

    add routing for ProjectDetailsPage and enable route reuse for HomePage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import {Routes} from '@angular/router';
 import {HomePage} from './features/home-page/home-page';
+import {ProjectDetailsPage} from './features/projects/project-details-page/project-details-page';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomePage
+    component: HomePage,
+    data: {reuse: true}
+  },
+  {
+    path: 'projects/:id',
+    component: ProjectDetailsPage
   }
 
 ];
