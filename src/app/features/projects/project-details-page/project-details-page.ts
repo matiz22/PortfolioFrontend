aeb27b4895edaa9276d8b1e6ec8b464a983133c3
matiz22 <mateuszmalich@gmail.com>
2025-09-01 13:44:03 +0200

    add ProjectDetailsPage component with template, styles, and tests

---
+++ src/app/features/projects/project-details-page/project-details-page.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-project-details-page',
+  imports: [],
+  templateUrl: './project-details-page.html',
+  styleUrl: './project-details-page.scss'
+})
+export class ProjectDetailsPage {
+
+}
