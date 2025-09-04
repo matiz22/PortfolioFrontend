7b415da0bfe74d25539a22668adbe8173f62f5c5
matiz22 <mateuszmalich@gmail.com>
2025-09-04 13:31:31 +0200

    inject NavigationService into HomePage and implement scrollToAbout method

--- src/app/features/home-page/home-page.ts
+++ src/app/features/home-page/home-page.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {HeroSection} from '../../shared/hero/hero-section/hero-section';
 import {AboutMe} from '../about-me/about-me';
 import {MatDivider} from '@angular/material/divider';
 import {MatIconButton} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {ProjectsSection} from '../projects/projects-section/projects-section';
+import {NavigationService} from '../../core/services/navigation.service';
 
 
 @Component({
@@ -23,4 +24,10 @@ import {ProjectsSection} from '../projects/projects-section/projects-section';
 export class HomePage {
   cardTitle: string = $localize`:@@homeTitle:👋 Hi, I’m Mateusz — Mobile & Web Developer`;
   cardSubtitle: string = $localize`:@@homeSubtitle:Crafting simple, effective digital experiences.`;
+
+  navigationService: NavigationService = inject(NavigationService);
+
+  scrollToAbout(): void {
+    this.navigationService.navigateToSection('about')
+  }
 }
