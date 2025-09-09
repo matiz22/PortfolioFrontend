58a561a7df88492b2acecd161fd1966cdd312921
matiz22 <mateuszmalich@gmail.com>
2025-09-09 12:54:50 +0200

    update ItemDetailsPage to use subtitle input and enhance skills display with chips

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {HomePage} from './features/home-page/home-page';
 import {ProjectDetailsPage} from './features/projects/project-details-page/project-details-page';
 import {ProjectListPage} from './features/projects/project-list-page/project-list-page';
 import {RealizationDetailsPage} from './features/realizations/realization-details-page/realization-details-page';
+import {JobDetailsPage} from './features/jobs/job-details-page/job-details-page';
 
 export const routes: Routes = [
   {
@@ -21,5 +22,9 @@ export const routes: Routes = [
   {
     path: 'realization/:id',
     component: RealizationDetailsPage,
+  },
+  {
+    path: 'job/:id',
+    component: JobDetailsPage,
   },
 ];
