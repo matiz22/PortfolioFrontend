49e551d0edd5d153d55a50f8bac72cd4f83530b5
matiz22 <mateuszmalich@gmail.com>
2025-09-09 10:01:41 +0200

    add JobsService with CRUD operations and home page integration

---
+++ src/app/core/services/jobs.service.ts
@@ -0,0 +1,48 @@
+import {Injectable} from '@angular/core';
+import {ICrudService} from './base/services/crud.service';
+import {IHomeService} from './base/services/home.service';
+import {CrudOperations} from './base/operations/crud.operations';
+import {HomeOperations} from './base/operations/home.operations';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../enviroments/enviroment';
+import {Observable} from 'rxjs';
+import {Job} from '../models/job';
+import {mapJob} from '../mappers/job';
+import {JobDto} from '../dto/job';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobsService implements ICrudService<Job>,
+  IHomeService<Job> {
+
+  private readonly baseUrl: string;
+  private readonly homeUrl: string;
+  private readonly entityName = 'Job';
+
+  private crudOps: CrudOperations<JobDto, Job>;
+  private homeOps: HomeOperations<JobDto, Job>;
+
+
+  constructor(private http: HttpClient) {
+    this.baseUrl = `${environment.apiUrl}/${environment.version}/jobs/translated`;
+    this.homeUrl = `${this.baseUrl}/home-page`;
+
+    this.crudOps = new CrudOperations(http, this.baseUrl, mapJob, this.entityName);
+    this.homeOps = new HomeOperations(http, this.homeUrl, mapJob, this.entityName);
+
+  }
+
+  getAll(): Observable<Job[]> {
+    return this.crudOps.getAll();
+  }
+
+  getById(id: string): Observable<Job | null> {
+    return this.crudOps.getById(id);
+  }
+
+  getHomeItems(): Observable<Job[]> {
+    return this.homeOps.getHomeItems();
+  }
+
+}
