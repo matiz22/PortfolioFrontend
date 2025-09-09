1ecfbbc50b3c3a45a622a4db7533513d43dafabf
matiz22 <mateuszmalich@gmail.com>
2025-09-09 13:08:36 +0200

    refactor project-list-page to use ProjectItem component for improved structure

--- src/app/features/projects/project-list-page/project-list-page.ts
+++ src/app/features/projects/project-list-page/project-list-page.ts
@@ -4,20 +4,14 @@ import {ProjectsService} from '../../../core/services/projects.service';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {Project} from '../../../core/models/project';
 import {MatIconModule} from '@angular/material/icon';
-import {ImageUrlPipe} from '../../../shared/pipes/image-url-pipe';
-import {MatChip, MatChipAvatar, MatChipSet} from '@angular/material/chips';
-import {ItemCard} from '../../../shared/showcase/item-card/item-card';
+import {ProjectItem} from '../project-item/project-item';
 
 @Component({
   selector: 'app-project-list-page',
   imports: [
     HeroSection,
     MatIconModule,
-    ItemCard,
-    ImageUrlPipe,
-    MatChip,
-    MatChipAvatar,
-    MatChipSet,
+    ProjectItem,
   ],
   templateUrl: './project-list-page.html',
   styleUrl: './project-list-page.scss'
