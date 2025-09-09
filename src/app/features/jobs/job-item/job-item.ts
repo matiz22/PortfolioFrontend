8d970672dca8ad196d4ce92c0e9625803a24a725
matiz22 <mateuszmalich@gmail.com>
2025-09-09 12:58:51 +0200

    refactor JobsSection to use JobItem component and separate job display logic

---
+++ src/app/features/jobs/job-item/job-item.ts
@@ -0,0 +1,25 @@
+import {Component, Input} from '@angular/core';
+import {DatePipe} from "@angular/common";
+import {ImageUrlPipe} from "../../../shared/pipes/image-url-pipe";
+import {ItemCard} from "../../../shared/showcase/item-card/item-card";
+import {MatChip, MatChipAvatar, MatChipSet} from "@angular/material/chips";
+import {Job} from '../../../core/models/job';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-job-item',
+  imports: [
+    DatePipe,
+    ImageUrlPipe,
+    ItemCard,
+    MatChip,
+    MatChipAvatar,
+    MatChipSet,
+    MatIconModule
+  ],
+  templateUrl: './job-item.html',
+  styleUrl: './job-item.scss'
+})
+export class JobItem {
+  @Input() job!: Job;
+}
--- src/app/features/jobs/jobs-section/jobs-section.ts
+++ src/app/features/jobs/jobs-section/jobs-section.ts
@@ -1,23 +1,15 @@
 import {Component, inject} from '@angular/core';
-import {ImageUrlPipe} from '../../../shared/pipes/image-url-pipe';
-import {ItemCard} from '../../../shared/showcase/item-card/item-card';
-import {MatChip, MatChipAvatar, MatChipSet} from '@angular/material/chips';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {JobsService} from '../../../core/services/jobs.service';
 import {Job} from '../../../core/models/job';
-import {DatePipe} from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
+import {JobItem} from '../job-item/job-item';
 
 @Component({
   selector: 'app-jobs-section',
   imports: [
-    ImageUrlPipe,
-    ItemCard,
-    MatChip,
-    MatChipAvatar,
-    MatChipSet,
-    DatePipe,
-    MatIconModule
+    MatIconModule,
+    JobItem
   ],
   templateUrl: './jobs-section.html',
   styleUrl: './jobs-section.scss'
