8bbc114b20d8eff3c052218b146f95f04645e07a
matiz22 <mateuszmalich@gmail.com>
2025-09-11 17:45:20 +0200

    add: implement SocialLinksService with CRUD operations and unit tests

---
+++ src/app/core/services/social.links.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from '@angular/core';
+import {ICrudService} from './base/services/crud.service';
+import {CrudOperations} from './base/operations/crud.operations';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../enviroments/enviroment';
+import {Observable} from 'rxjs';
+import {SocialLink} from '../models/social.link';
+import {SocialLinkDto} from '../dto/social.link';
+import {mapSocialLink} from '../mappers/social.link';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocialLinksService implements ICrudService<SocialLink> {
+
+  private readonly baseUrl: string;
+  private readonly entityName = 'SocialLink';
+
+  private crudOps: CrudOperations<SocialLinkDto, SocialLink>;
+
+
+  constructor(private http: HttpClient) {
+    this.baseUrl = `${environment.apiUrl}/${environment.version}/social-links/translated`;
+
+    this.crudOps = new CrudOperations(http, this.baseUrl, mapSocialLink, this.entityName);
+
+  }
+
+  getAll(): Observable<SocialLink[]> {
+    return this.crudOps.getAll();
+  }
+
+  getById(id: string): Observable<SocialLink | null> {
+    return this.crudOps.getById(id);
+  }
+
+}
