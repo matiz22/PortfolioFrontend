d1b4cd3b6cce24a86e62222cf89acc0e67515d21
matiz22 <mateuszmalich@gmail.com>
2025-09-11 15:37:09 +0200

    add: implement ContactService with email sending functionality and unit tests

---
+++ src/app/core/services/contact.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {environment} from '../../../enviroments/enviroment';
+import {Contact} from '../models/contact';
+import {catchError, map, Observable, of} from 'rxjs';
+import {mapContactDto} from '../mappers/contact.dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+  private readonly baseUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.baseUrl = `${environment.apiUrl}/${environment.version}/send-email`;
+  }
+
+  sendEmail(contact: Contact): Observable<boolean> {
+    if (contact.company && contact.company.trim().length > 0) {
+      return of(true);
+    }
+
+    return this.http.post<any>(this.baseUrl, mapContactDto(contact, 'contact_form')).pipe(
+      map(() => true),
+      catchError(err => this.handleError(err))
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<boolean> {
+    console.error(error);
+    return of(false);
+  }
+}
