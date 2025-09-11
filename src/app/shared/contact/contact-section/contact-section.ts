b3256f165f4e6d5f6b5baf167cfd41a9ad465cb3
matiz22 <mateuszmalich@gmail.com>
2025-09-11 15:49:37 +0200

    add: enhance contact section with divider and adjust styling

--- src/app/shared/contact/contact-section/contact-section.ts
+++ src/app/shared/contact/contact-section/contact-section.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {SectionTexts} from '../../sections/section-texts/section-texts';
 import {ContactForm} from '../contact-form/contact-form';
+import {MatDivider} from '@angular/material/divider';
 
 @Component({
   selector: 'app-contact-section',
   imports: [
     SectionTexts,
-    ContactForm
+    ContactForm,
+    MatDivider
   ],
   templateUrl: './contact-section.html',
   styleUrl: './contact-section.scss'
