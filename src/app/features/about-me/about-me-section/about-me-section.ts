8c538f9acb1493ad5b052d1edc336e3c78e4b6cc
matiz22 <mateuszmalich@gmail.com>
2025-09-13 10:48:08 +0200

    add: create About Me section with component, template, and styles

---
+++ src/app/features/about-me/about-me-section/about-me-section.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {MatFabButton} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-about-me-section',
+  imports: [
+    MatFabButton,
+    RouterLink,
+    MatIconModule
+  ],
+  templateUrl: './about-me-section.html',
+  styleUrl: './about-me-section.scss'
+})
+export class AboutMeSection {
+
+}
