27f43bd05084be8208878805745c76179fcf29d8
matiz22 <mateuszmalich@gmail.com>
2025-09-28 13:09:43 +0200

    new about me page

--- src/app/features/about-me/about-me-page/about-me-page.ts
+++ src/app/features/about-me/about-me-page/about-me-page.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
 import {HeroSection} from '../../../shared/hero/hero-section/hero-section';
+import {ContactSection} from '../../../shared/contact/contact-section/contact-section';
 
 @Component({
   selector: 'app-about-me-page',
   imports: [
-    HeroSection
+    HeroSection,
+    ContactSection
   ],
   templateUrl: './about-me-page.html',
   styleUrl: './about-me-page.scss'
