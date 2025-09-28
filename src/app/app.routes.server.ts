a41e365bd32d240d01f00b8c6195966c5e709be0
matiz22 <mateuszmalich@gmail.com>
2025-09-28 17:25:47 +0200

    Add routes for Privacy Policy and About Me pages with prerender mode

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import {RenderMode, ServerRoute} from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'privacy-policy',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'about-me',
+    renderMode: RenderMode.Prerender
+  },
+  {
     path: '**',
     renderMode: RenderMode.Server
   }
