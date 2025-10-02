acfca99e73ccfca5190bff3962b7c53f374ee7fa
matiz22 <mateuszmalich@gmail.com>
2025-10-02 20:26:36 +0200

    Add language options to environment configuration

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,6 +1,20 @@
+import {LanguageOption} from '../app/core/models/languages';
+
 export const environment = {
   production: false,
   apiUrl: 'http://127.0.0.1:8000/api',
   storageUrl: 'http://127.0.0.1:8000/storage',
-  version: 'v1'
+  version: 'v1',
+  languagesOptions: [
+    {
+      code: 'en',
+      label: 'English',
+      url: 'http://localhost:4000/en/'
+    },
+    {
+      code: 'pl',
+      label: 'Polski',
+      url: 'http://localhost:4000/pl/'
+    },
+  ] as LanguageOption[]
 };
