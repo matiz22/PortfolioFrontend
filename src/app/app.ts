ebc77979d196db003f8dac98ac846fdd9310707e
matiz22 <mateuszmalich@gmail.com>
2025-10-02 20:27:16 +0200

    Refactor footer animation and update app component for language support

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,17 @@
-import {Component, signal, ViewChild} from '@angular/core';
+import {Component, Inject, LOCALE_ID, signal, ViewChild} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatSidenav, MatSidenavContainer, MatSidenavContent,} from '@angular/material/sidenav';
 import {MatListItem, MatNavList} from '@angular/material/list';
-import {MatIconButton} from '@angular/material/button';
+import {MatButton, MatIconButton} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {Footer} from './shared/footer/footer/footer';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
+import {environment} from '../enviroments/enviroment';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbar, MatToolbar, MatNavList, MatListItem, MatIconButton, MatIconModule, MatSidenavContainer, MatSidenav, MatSidenavContent, Footer, RouterLink],
+  imports: [RouterOutlet, MatToolbar, MatToolbar, MatNavList, MatListItem, MatIconButton, MatIconModule, MatSidenavContainer, MatSidenav, MatSidenavContent, Footer, RouterLink, MatMenu, MatButton, MatMenuItem, MatMenuTrigger],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -18,12 +20,17 @@ export class App {
 
   @ViewChild('drawer') drawer!: MatSidenav;
 
-  constructor() {
-
-  }
+  protected readonly environment = environment;
 
   toggleDrawer(): void {
     this.drawer.toggle();
   }
 
+  constructor(@Inject(LOCALE_ID) public locale: string) {
+
+  }
+
+  getCurrentLanguage(): string {
+    return environment.languagesOptions.find(lang => lang.code === this.locale)?.label || 'Unknown';
+  }
 }
