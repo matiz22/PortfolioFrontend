54eb23dacb6624e180c7de2c7af1e004cc04b142
matiz22 <mateuszmalich@gmail.com>
2025-10-14 18:34:59 +0200

    replace text message with email sent confirmation component in contact form

--- src/app/shared/contact/contact-form/contact-form.ts
+++ src/app/shared/contact/contact-form/contact-form.ts
@@ -5,6 +5,7 @@ import {MatButton} from '@angular/material/button';
 import {MatInput} from '@angular/material/input';
 import {Contact} from '../../../core/models/contact';
 import {ContactService} from '../../../core/services/contact.service';
+import {EmaiSent} from '../emai-sent/emai-sent';
 
 @Component({
   selector: 'app-contact-form',
@@ -15,7 +16,8 @@ import {ContactService} from '../../../core/services/contact.service';
     MatError,
     ReactiveFormsModule,
     MatButton,
-    MatInput
+    MatInput,
+    EmaiSent
   ],
   styleUrls: ['./contact-form.scss']
 })
