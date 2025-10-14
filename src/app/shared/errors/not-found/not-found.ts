f17de97bc087023cc614ec214d3e068f94df4335
matiz22 <mateuszmalich@gmail.com>
2025-10-14 20:00:16 +0200

    add not found component with template, styles, and tests

---
+++ src/app/shared/errors/not-found/not-found.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [
+    MatIconModule
+  ],
+  templateUrl: './not-found.html',
+  styleUrl: './not-found.scss'
+})
+export class NotFound {
+
+}
