90e27980406c469dd8988e99c8d7872d61eb2bf5
matiz22 <mateuszmalich@gmail.com>
2025-11-26 08:58:52 +0100

    add custom route reuse strategy for optimized navigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,28 @@
 import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import {provideRouter, RouteReuseStrategy, withInMemoryScrolling, withViewTransitions} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi} from '@angular/common/http';
+import {LocaleInterceptor} from './core/interceptors/locale.interceptor';
+import {CustomRouteReuseStrategy} from './route.reuse.strategy';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({eventCoalescing: true}),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(
+      routes,
+      withViewTransitions(),
+      withInMemoryScrolling({scrollPositionRestoration: 'enabled'}),
+    ),
+    {provide: RouteReuseStrategy, useClass: CustomRouteReuseStrategy},
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(
+      withFetch(),
+      withInterceptorsFromDi()
+    ),
+    {provide: HTTP_INTERCEPTORS, useClass: LocaleInterceptor, multi: true}
   ]
 };
