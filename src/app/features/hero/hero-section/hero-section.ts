caeb36fc21ea8427d73c6aa98fefda57ddd24ebf
matiz22 <mateuszmalich@gmail.com>
2025-12-06 11:32:01 +0100

    add HeroSection component with template, styles, and tests

---
+++ src/app/features/hero/hero-section/hero-section.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {NgxAuroraComponent} from "@omnedia/ngx-aurora";
+
+@Component({
+  selector: 'app-hero-section',
+  imports: [
+    NgxAuroraComponent
+  ],
+  templateUrl: './hero-section.html',
+  styleUrl: './hero-section.scss',
+})
+export class HeroSection {
+
+}
