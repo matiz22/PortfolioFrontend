34fc4c20854a0a3a170e35179074e53a5dc2e470
matiz22 <mateuszmalich@gmail.com>
2025-12-06 11:31:26 +0100

    add CertificationItem component with template, styles, and tests

---
+++ src/app/features/certifications/certification-item/certification-item.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-certification-item',
+  imports: [],
+  templateUrl: './certification-item.html',
+  styleUrl: './certification-item.scss',
+})
+export class CertificationItem {
+
+}
