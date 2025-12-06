263c08d995dc25a5f2ab78552a71b66bfd7fbd0b
matiz22 <mateuszmalich@gmail.com>
2025-12-06 11:31:32 +0100

    add CertificationSection component with template, styles, and tests

---
+++ src/app/features/certifications/certification-section/certification-section.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-certification-section',
+  imports: [],
+  templateUrl: './certification-section.html',
+  styleUrl: './certification-section.scss',
+})
+export class CertificationSection {
+
+}
