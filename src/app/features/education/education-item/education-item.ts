d6a02b2a9a189dfd197e2729d06dc3f68c6988c3
matiz22 <mateuszmalich@gmail.com>
2025-12-06 11:31:40 +0100

    add EducationItem component with template, styles, and tests

---
+++ src/app/features/education/education-item/education-item.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-education-item',
+  imports: [],
+  templateUrl: './education-item.html',
+  styleUrl: './education-item.scss',
+})
+export class EducationItem {
+
+}
