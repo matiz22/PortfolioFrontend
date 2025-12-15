61382ea378041399f04b17ceaad90ba4af0de713
matiz22 <mateuszmalich@gmail.com>
2025-12-15 22:26:03 +0100

    refactor Education section to use EducationSection component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,19 +2,17 @@ import {Component, signal} from '@angular/core';
 import {AboutMeSection} from './features/about-me/about-me-section/about-me-section';
 import {Header} from './shared/header/header';
 import {HeroSection} from './features/hero/hero-section/hero-section';
-import {ProjectItem} from './features/projects/project-item/project-item';
-import {RealizationItem} from './features/realizations/realization-item/realization-item';
 import {JobTimelineSection} from './features/jobs/job-timeline-section/job-timeline-section';
-import {EducationItem} from './features/education/education-item/education-item';
 import {CertificationItem} from './features/certifications/certification-item/certification-item';
 import {Footer} from './shared/footer/footer';
 import {
   ProjectsRealizationsHomeSection
 } from './features/showcase/projects-realizations-home-section/projects-realizations-home-section';
+import {EducationSection} from './features/education/education-section/education-section';
 
 @Component({
   selector: 'app-root',
-  imports: [AboutMeSection, Header, HeroSection, ProjectItem, RealizationItem, JobTimelineSection, EducationItem, CertificationItem, Footer, ProjectsRealizationsHomeSection],
+  imports: [AboutMeSection, Header, HeroSection, JobTimelineSection, CertificationItem, Footer, ProjectsRealizationsHomeSection, EducationSection],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
