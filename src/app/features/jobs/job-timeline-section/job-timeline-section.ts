68f4e9d9dbec2533286c09c6c3c10a7d49f4c8f7
matiz22 <mateuszmalich@gmail.com>
2025-12-15 17:48:28 +0100

    add JobTimelineSection component with dynamic job loading and error handling

--- src/app/features/jobs/job-timeline-section/job-timeline-section.ts
+++ src/app/features/jobs/job-timeline-section/job-timeline-section.ts
@@ -1,11 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {DatePipe} from '@angular/common';
+import {ApiState} from '../../../core/models/api.state';
+import {JobsService} from '../../../core/services/jobs.service';
+import {Job} from '../../../core/models/job';
 
 @Component({
   selector: 'app-job-timeline-section',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './job-timeline-section.html',
   styleUrl: './job-timeline-section.scss',
 })
 export class JobTimelineSection {
-
+  jobsService: JobsService = inject(JobsService);
+  jobs = toSignal(
+    this.jobsService.getHomeItems(),
+    {
+      initialValue: ApiState.loading<Job[]>()
+    }
+  );
 }
