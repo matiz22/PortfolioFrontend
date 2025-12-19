036ef938860faf6766fddc31b9bc84d249292ddc
matiz22 <mateuszmalich@gmail.com>
2025-12-19 20:45:34 +0100

    feat: update Home navigation link to 'projects' route

--- src/app/features/projects/projects-page/projects-page.ts
+++ src/app/features/projects/projects-page/projects-page.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProjectsService } from '../../../core/services/projects.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ApiState } from '../../../core/models/api.state';
+import { Project } from '../../../core/models/project';
+import { ProjectItem } from '../project-item/project-item';
+import { Header } from '../../../shared/header/header';
+import { ContactSection } from '../../../shared/contact/contact-section/contact-section';
+import { Footer } from '../../../shared/footer/footer';
 
 @Component({
   selector: 'app-projects-page',
-  imports: [],
+  imports: [
+    ProjectItem,
+    Header,
+    ContactSection,
+    Footer
+  ],
   templateUrl: './projects-page.html',
   styleUrl: './projects-page.scss',
 })
 export class ProjectsPage {
-
+  projectsService: ProjectsService = inject(ProjectsService);
+  projects = toSignal(
+    this.projectsService.getAll(),
+    {
+      initialValue: ApiState.loading<Project[]>()
+    }
+  );
 }
