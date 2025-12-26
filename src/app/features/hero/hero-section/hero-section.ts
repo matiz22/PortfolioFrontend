0b91bc61033a2b1eca7f353df06b50fa190bc7f4
matiz22 <mateuszmalich@gmail.com>
2025-12-26 22:10:59 +0100

    feat: implement a visually enhanced hero section with animated backgrounds, gradient text, and updated CTAs.

--- src/app/features/hero/hero-section/hero-section.ts
+++ src/app/features/hero/hero-section/hero-section.ts
@@ -1,10 +1,10 @@
-import {Component} from '@angular/core';
-import {NgxAuroraComponent} from "@omnedia/ngx-aurora";
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero-section',
   imports: [
-    NgxAuroraComponent
+    RouterLink
   ],
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.scss',
