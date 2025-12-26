cd8616f047be6b09c973ecccd9a4210bfddd6620
matiz22 <mateuszmalich@gmail.com>
2025-12-26 20:13:19 +0100

    feat: Add navigation to certification item cards and prevent credential link propagation

--- src/app/features/certifications/certification-item/certification-item.ts
+++ src/app/features/certifications/certification-item/certification-item.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { Certification } from '../../../core/models/certification';
 import { ImageUrlPipe } from '../../../shared/pipes/image-url-pipe';
 
 @Component({
   selector: 'app-certification-item',
-  imports: [DatePipe, ImageUrlPipe],
+  imports: [DatePipe, ImageUrlPipe, RouterLink],
   templateUrl: './certification-item.html',
   styleUrl: './certification-item.scss',
 })
