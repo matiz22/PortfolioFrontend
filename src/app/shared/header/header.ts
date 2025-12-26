49aa9a5cf6644c9b8abfcf3e5b7022d1e8f63147
matiz22 <mateuszmalich@gmail.com>
2025-12-26 23:00:59 +0100

    feat: Add logic to close mobile menu on outside clicks

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -10,5 +10,10 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.scss',
 })
 export class Header {
+  @ViewChild('menuDetails') menuDetails!: ElementRef<HTMLDetailsElement>;
 
+  @HostListener('document:click')
+  closeOnAnyClick() {
+    this.menuDetails?.nativeElement.removeAttribute('open');
+  }
 }
