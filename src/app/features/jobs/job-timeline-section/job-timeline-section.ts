bef25ac51ed70ec8f15f2bf902cb021c49f6d38c
matiz22 <mateuszmalich@gmail.com>
2025-12-26 21:17:27 +0100

    feat: Add `routerLink` to job timeline items for navigation and enhance their hover styling.

--- src/app/features/jobs/job-timeline-section/job-timeline-section.ts
+++ src/app/features/jobs/job-timeline-section/job-timeline-section.ts
@@ -1,13 +1,14 @@
-import {Component, inject} from '@angular/core';
-import {toSignal} from '@angular/core/rxjs-interop';
-import {DatePipe} from '@angular/common';
-import {ApiState} from '../../../core/models/api.state';
-import {JobsService} from '../../../core/services/jobs.service';
-import {Job} from '../../../core/models/job';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ApiState } from '../../../core/models/api.state';
+import { JobsService } from '../../../core/services/jobs.service';
+import { Job } from '../../../core/models/job';
 
 @Component({
   selector: 'app-job-timeline-section',
-  imports: [DatePipe],
+  imports: [DatePipe, RouterLink],
   templateUrl: './job-timeline-section.html',
   styleUrl: './job-timeline-section.scss',
 })
