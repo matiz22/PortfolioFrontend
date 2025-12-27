17abcefc1c9faa9510b21886bb2595be853c526d
matiz22 <mateuszmalich@gmail.com>
2025-12-27 19:16:31 +0100

    feat: Redesign contact form with new styling, integrate social links into the contact section, and add global background decorations.

--- src/app/shared/contact/contact-section/contact-section.ts
+++ src/app/shared/contact/contact-section/contact-section.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ContactForm } from "../contact-form/contact-form";
+import { SocialLinksRow } from "../../../features/social-links/social-links-row/social-links-row";
 
 @Component({
   selector: 'app-contact-section',
-  imports: [ContactForm],
+  imports: [ContactForm, SocialLinksRow],
   templateUrl: './contact-section.html',
   styleUrl: './contact-section.scss',
 })
