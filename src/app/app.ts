71405291dd85914361937806307bfac4205532a6
matiz22 <mateuszmalich@gmail.com>
2025-12-28 16:52:53 +0100

    feat: add ViewportScroller and set offset for fixed header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,11 @@ import { RouterOutlet } from "@angular/router";
   styleUrl: './app.scss'
 })
 export class App {
+  private viewportScroller = inject(ViewportScroller);
   protected readonly title = signal('PortfolioFrontend');
+
+  constructor() {
+    this.viewportScroller.setOffset([0, 30]); // [x, y] offset for fixed header
+  }
 }
 
