70de2a372dc0a3e54b0092c98e693eab4424f011
matiz22 <mateuszmalich@gmail.com>
2025-12-29 01:19:33 +0100

    refactor: remove `startWith(ApiState.loading)` operator from CRUD and home operations.

--- src/app/core/services/base/operations/crud.operations.ts
+++ src/app/core/services/base/operations/crud.operations.ts
@@ -1,6 +1,6 @@
-import {catchError, map, Observable, of, startWith} from 'rxjs';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {ApiState} from '../../../models/api.state';
+import { catchError, map, Observable, of } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ApiState } from '../../../models/api.state';
 
 export class CrudOperations<TDto, TModel> {
   constructor(
@@ -14,7 +14,6 @@ export class CrudOperations<TDto, TModel> {
   getAll(): Observable<ApiState<TModel[]>> {
     return this.http.get<TDto[]>(this.baseUrl).pipe(
       map(dtos => ApiState.success(dtos.map(dto => this.mapper(dto)))),
-      startWith(ApiState.loading<TModel[]>()),
       catchError(err => of(ApiState.error<TModel[]>(this.getErrorMessage(err))))
     );
   }
@@ -22,7 +21,6 @@ export class CrudOperations<TDto, TModel> {
   getById(id: string): Observable<ApiState<TModel>> {
     return this.http.get<TDto>(`${this.baseUrl}/${id}`).pipe(
       map(dto => ApiState.success(this.mapper(dto))),
-      startWith(ApiState.loading<TModel>()),
       catchError(err => of(ApiState.error<TModel>(this.getErrorMessage(err))))
     );
   }
--- src/app/core/services/base/operations/home.operations.ts
+++ src/app/core/services/base/operations/home.operations.ts
@@ -1,6 +1,6 @@
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {catchError, map, Observable, of, startWith} from 'rxjs';
-import {ApiState} from '../../../models/api.state';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, map, Observable, of } from 'rxjs';
+import { ApiState } from '../../../models/api.state';
 
 export class HomeOperations<TDto, TModel> {
   constructor(
@@ -14,7 +14,6 @@ export class HomeOperations<TDto, TModel> {
   getHomeItems(): Observable<ApiState<TModel[]>> {
     return this.http.get<TDto[]>(this.homeUrl).pipe(
       map(dtos => ApiState.success(dtos.map(dto => this.mapper(dto)))),
-      startWith(ApiState.loading<TModel[]>()),
       catchError(err => of(ApiState.error<TModel[]>(this.getErrorMessage(err))))
     );
   }
