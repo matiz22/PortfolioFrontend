ad9fe3bd8fd918e128ea5df7e379064da0d91e78
matiz22 <mateuszmalich@gmail.com>
2026-02-05 10:59:19 +0100

    Add LoadingCard skeleton component

---
+++ src/app/shared/loading/loading-card/loading-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-card',
+  imports: [],
+  templateUrl: './loading-card.html',
+  styleUrl: './loading-card.scss',
+})
+export class LoadingCard {
+
+}
